2a3f555637c77e0da4bb85938d0f0acd90d9ff28
kenneth.marete <maretekent@gmail.com>
2019-10-10 20:27:24 +0300

    Add static files on urls

--- configurations/urls.py
+++ configurations/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.views import serve
 
 from app_dir.authentication.urls import urlpatterns as authentication_urls
 
@@ -22,3 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include(authentication_urls)),
 ]
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
